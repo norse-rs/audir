3493c93b1fa5d5380442b7f480d6146251a79f75
msiglreith <m.siglreith@gmail.com>
2019-09-16 12:12:15 +0200

    coreaudio: setup skeleton

--- src/coreaudio/mod.rs
+++ src/coreaudio/mod.rs
@@ -1,8 +1,42 @@
+use crate::{DeviceProperties, SampleDesc};
 
 pub struct Instance {}
 
 impl Instance {
     pub unsafe fn create(_name: &str) -> Self {
         Instance { }
+    }
+
+    pub unsafe fn enumerate_physical_output_devices(&self) -> Vec<PhysicalDevice> {
+        unimplemented!()
+    }
+
+    pub unsafe fn enumerate_physical_input_devices(&self) -> Vec<PhysicalDevice> {
+        unimplemented!()
+    }
+
+    pub unsafe fn create_device(
+        &self,
+        physical_device: &PhysicalDevice,
+        sample_desc: SampleDesc,
+    ) -> Device {
+        Device
+    }
+
+    pub unsafe properties(&self) -> DeviceProperties {
+        unimplemented!()
+    }
+}
+
+pub struct PhysicalDevice;
+
+pub struct Device;
+
+impl Device {
+    pub unsafe fn output_stream<F>(&self, callback: Option<F>)
+    where
+        F: FnMut(*mut (), usize),
+    {
+        unimplemented!()
     }
 }
