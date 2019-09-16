ec9ea2ef2c8e17239bbee8ab2c6c280c508167b6
msiglreith <m.siglreith@gmail.com>
2019-09-16 13:37:31 +0200

    coreaudio: physical device enumeration

--- src/coreaudio/mod.rs
+++ src/coreaudio/mod.rs
@@ -1,4 +1,43 @@
 use crate::{DeviceProperties, SampleDesc};
+use coreaudio_sys as ca;
+use std::ptr;
+
+unsafe fn get_property_data(
+    id: ca::AudioObjectID,
+    address: &ca::AudioObjectPropertyAddress,
+    data: *mut (),
+    len: u32,
+) -> u32 {
+    let mut size = len;
+    let status = ca::AudioObjectGetPropertyData(
+        id,
+        address,
+        0,
+        ptr::null_mut(),
+        &mut size,
+        data as _,
+    );
+    assert_eq!(status, 0);
+
+    size
+}
+
+unsafe fn get_property_data_size(
+    id: ca::AudioObjectID,
+    address: &ca::AudioObjectPropertyAddress,
+) -> u32 {
+    let mut size = 0;
+    let status = ca::AudioObjectGetPropertyDataSize(
+        id,
+        address,
+        0,
+        ptr::null_mut(),
+        &mut size
+    );
+    assert_eq!(status, 0);
+
+    size
+}
 
 pub struct Instance {}
 
@@ -7,11 +46,32 @@ impl Instance {
         Instance { }
     }
 
+    unsafe fn enumerate_physical_devices(&self) -> Vec<ca::AudioObjectID> {
+        let address = ca::AudioObjectPropertyAddress {
+            mElement: ca::kAudioObjectPropertyElementMaster,
+            mScope: ca::kAudioObjectPropertyScopeGlobal,
+            mSelector: ca::kAudioHardwarePropertyDevices,
+        };
+
+        let id = ca::kAudioObjectSystemObject;
+        let size = get_property_data_size(id, &address);
+        let num_elements = size / mem::size_of::<ca::AudioObjectID>();
+        let mut data = Vec::with_capacity(num_elements);
+        data.set_len(num_elements);
+        get_property_data(id, &address, data.as_mut_ptr() as _, size);
+
+        data
+    }
+
     pub unsafe fn enumerate_physical_output_devices(&self) -> Vec<PhysicalDevice> {
+        let audio_objects = self.enumerate_physical_devices();
+
         unimplemented!()
     }
 
     pub unsafe fn enumerate_physical_input_devices(&self) -> Vec<PhysicalDevice> {
+        let audio_objects = self.enumerate_physical_devices();
+
         unimplemented!()
     }
 
@@ -23,7 +83,7 @@ impl Instance {
         Device
     }
 
-    pub unsafe properties(&self) -> DeviceProperties {
+    pub unsafe fn properties(&self) -> DeviceProperties {
         unimplemented!()
     }
 }
